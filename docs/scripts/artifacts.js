925132f3646e3c54c67aefe4bd79f7576b391bed
Jared Tham <jared.tham@hotmail.com>
2025-07-28 11:21:10 +0800

    Simplified artifact class, still figuring out fetching

--- docs/scripts/artifacts.js
+++ docs/scripts/artifacts.js
@@ -1,9 +1,9 @@
 const ARTIFACT_SLOTS = {
-    FLOWER: { id: "flower", displayName: "Flower of Life" },
-    PLUME: { id: "plume", displayName: "Plume of Death" },
-    SANDS: { id: "sands", displayName: "Sands of Eon" },
-    GOBLET: { id: "goblet", displayName: "Goblet of Eonothem" },
-    CIRCLET: { id: "circlet", displayName: "Circlet of Logos" },
+    flower: { displayName: "Flower of Life" },
+    plume: { displayName: "Plume of Death" },
+    sands: { displayName: "Sands of Eon" },
+    goblet: { displayName: "Goblet of Eonothem" },
+    circlet: { displayName: "Circlet of Logos" }
 };
 
 const generateButton = document.getElementById("generate-btn").addEventListener("click", generateAndDisplayArtifact);
@@ -13,13 +13,11 @@ const artifactOutput = document.getElementById("artifact-output");
 let artifactSets = null;
 
 class Artifact {
-    constructor(setKey, slotData, level = 0, mainStatKey, substats = []) {
-        this.setKey = setKey;                                   // EmblemOfSeveredFate
-        this.setName = artifactSets[setKey]["displayName"];     // Emblem of Severed Fate
-        this.slotKey = slotData["id"];                          // plumfe
-        this.slotName = slotData["displayName"];                // Plume of Death
-        this.level = level;                                     // 20
-        this.mainStatKey = mainStatKey;                         // ATK
+    constructor(setKey, slotKey, level = 0, mainStatKey, substats = []) {
+        this.setKey = setKey;               // EmblemOfSeveredFate
+        this.slotKey = slotKey;             // plume
+        this.level = level;                 // 20
+        this.mainStatKey = mainStatKey;     // ATK
         this.substats = substats;
     }
 }
@@ -52,14 +50,17 @@ function generateAndDisplayArtifact() {
 }
 
 function generateArtifact(setKey) {
-    const slots = Object.values(ARTIFACT_SLOTS);
-    const slotData = slots[Math.floor(Math.random() * slots.length)];
+    const slots = Object.keys(ARTIFACT_SLOTS);
+    const slotKey = slots[Math.floor(Math.random() * slots.length)];
 
 
-    return new Artifact(setKey, slotData, 0, "HP", []);
+    return new Artifact(setKey, slotKey, 0, "HP", []);
 }
 
 function displayArtifact(artifact) {
+    const setName = artifactSets[artifact.setKey]["displayName"];
+    const slotName = ARTIFACT_SLOTS[artifact.slotKey]["displayName"];
+    
     console.log(artifact);
 }
 
